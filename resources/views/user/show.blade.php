a2bdc0a4733b2e7a6bbbf4623c08feb3eaa06565
Mattia Pedone <mattiapedone93@gmail.com>
2020-03-26 18:51:14 +0100

    up

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,12 +1,11 @@
 @extends('user.layouts.layout')
   @section('main-content')
-<h2 class="text-center text-warning">Utente: {{$user->name}}</h2>
-    <div class="card">
-      <ul>
-        @foreach ($user->photos as $photo)
-              <li>{{ $photo->path }}</li>
-          @endforeach
-          
-      </ul>
-    </div>            
+    <h2 class="text-center text-warning">Utente: {{$user->name}}</h2>
+        <div class="card">
+          <ul>
+            @foreach ($user->photos as $photo)
+              <li class="text-primary">TITOLO FOTO: <span class="text-dark">{{ $photo->title }}</span></li>
+            @endforeach
+          </ul>
+        </div>            
   @endsection
