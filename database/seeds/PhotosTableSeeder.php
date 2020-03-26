537f34fb52b67461367b2e72c07c28b5910c99ce
Mattia Pedone <mattiapedone93@gmail.com>
2020-03-26 16:12:48 +0100

    up: ciclo creazione foto e collegamento agli user

---
+++ database/seeds/PhotosTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Photo;
+
+class PhotosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 18; $i++) {
+            $randNumber = rand(1, 3);
+            $newPhoto = new Photo;
+            $newPhoto->title = $faker->sentence($nbWords = 15, $variableNbWords = true);
+            $newPhoto->path = $faker->imageUrl($width = 640, $height = 480);
+            $newPhoto->user_id = $randNumber;
+            $newPhoto->save();
+        }
+    }
+}
